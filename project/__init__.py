fe4dc3b0e68434aaabf9ce0b63df391ecb5ab898
Martin Ansong <martinansong@Martins-MacBook-Pro.local>
2017-11-12 12:45:44 +0000

    add ropeproject to .gitignore

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,7 @@
 import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # we initiate the application
 app = Flask(__name__)
@@ -8,11 +10,9 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
-print(app.config)
-
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
-        'status':'success',
-        'message':'pong!'
+        'status': 'success',
+        'message': 'pong!'
 })
