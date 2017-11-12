4f397f35dd62b2ed667166169900b9f6de462530
Martin Ansong <martinansong@Martins-MacBook-Pro.local>
2017-11-12 12:29:33 +0000

    update config.py with sqlalchemy database uris

--- project/config.py
+++ project/config.py
@@ -1,18 +1,23 @@
 class BaseConfig:
     """docstring for BaseConfig."""
     """   Base Configuration    """
-    DEBUG   = False
-    TESTING = False
+    DEBUG                          = False
+    TESTING                        = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     """docstring for DevelopmentConfig."""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 
 class TestingConfig(BaseConfig):
     """docstring for TestingConfig."""
     DEBUG   = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URI')
+
 
 class ProductionConfig(BaseConfig):
     """docstring for ProductionConfig."""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
